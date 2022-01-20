dcbe5bb69ad50a24b104b0b971168a251d661029
Yusuf <bwitlawalyusuf@gmail.com>
2022-01-20 15:21:38 +0100

    Added car core features components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <promo></promo>
     <specifications></specifications>
     <interior></interior>
+    <core-features></core-features>
   </div>
 </template>
 
@@ -10,8 +11,9 @@
 import Promo from '../components/Promo.vue'
 import Specifications from '../components/Specifications.vue'
 import Interior from '../components/Interior.vue'
+import CoreFeatures from '../components/CoreFeatures.vue'
 export default {
-  components: { Promo, Specifications, Interior },
+  components: { Promo, Specifications, Interior, CoreFeatures },
 
 }
 </script>

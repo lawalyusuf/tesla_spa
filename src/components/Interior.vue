73a45916766be723acd3291c144eb701e6b79a62
Yusuf <bwitlawalyusuf@gmail.com>
2022-01-20 08:19:56 +0100

    Added the Interior components to home.vue

---
+++ src/components/Interior.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-img style="width:100%;" src="../../public/MX-Interior-Hero-Desktop.jpg"></v-img>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
   <div>
     <promo></promo>
     <specifications></specifications>
+    <interior></interior>
   </div>
 </template>
 
 <script>
 import Promo from '../components/Promo.vue'
 import Specifications from '../components/Specifications.vue'
+import Interior from '../components/Interior.vue'
 export default {
-  components: { Promo, Specifications},
+  components: { Promo, Specifications, Interior },
 
 }
 </script>
